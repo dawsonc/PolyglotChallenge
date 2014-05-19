40c2440d9155dc0c047a8be70b00baaecf1002dc
TheBritKnight <charles.dwsn@gmail.com>
2014-05-19 11:06:13 -0400

    SQL

---
+++ sql/count_imessages.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(*) FROM message; # Counts all the messages you've ever sent or received
+SELECT COUNT(*) FROM message WHERE handle_id = 1; # Counts all the messages you've ever exchanged
