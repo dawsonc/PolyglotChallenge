b753979c8e76ef01d4e4fb92beed68d3134c034e
Charles Dawson <charles.dwsn@gmail.com>
2014-07-16 19:50:31 -0400

    More bash!

--- bash/battery.bash
+++ bash/battery.bash
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # I added this on to the PS1 from bashstrap (https://github.com/barryclark/bashstrap)
 # but you can put it wherever you want
@@ -15,16 +15,23 @@ MAX_CAPACITY=$(ioreg -n AppleSmartBattery -r |
 # (Hack to do floating point math: delegate out to bc)
 # (scale=5 gives 5 decimal places of precision)
 BATTERY_FRAC=$(echo "scale=5; $CAPACITY/$MAX_CAPACITY" | bc)
+BATTERY_FRAC=$(printf "%.1f" "$BATTERY_FRAC") # Round to nearest 10th
 
-# Preferences for the battery status bar graph; set these to whatever looks nice
-MAX_NUM_BARS=6
-BAR_SYM="▮"
+MAX_NUM_BARS=10
+BAR_SYM="▮" # Set this to whatever you want. Go nuts
 
 # Construct bar graph
-BAR_STRING=""
-NUM_BARS=$(echo "scale=0; $BATTERY_FRAC*$MAX_NUM_BARS" | bc) # scale=0 forces integer output
+BATTERY_STRING=""
+NUM_BARS=$(echo "scale=0; $BATTERY_FRAC*($MAX_NUM_BARS - 1) + 1" | bc) # scale=0 forces integer output
 for i in `seq 1 $NUM_BARS`; do
-  BAR_STRING="$BAR_STRING$BAR_SYM" # Concatenate another bar onto the string
+  BATTERY_STRING="$BATTERY_STRING$BAR_SYM" # Concatenate another bar onto the string
 done
 
-echo "$BAR_STRING"
+# Append padding characters to make sure BATTERY_STRING has constant width
+PADDING_NEEDED=$(echo "scale=0; $MAX_NUM_BARS - $NUM_BARS" | bc)
+for i in `seq 0 $PADDING_NEEDED`; do
+  BATTERY_STRING="$BATTERY_STRING "
+done
+
+# Save this file to ~/bin/battery.bash and run "chmod 755 ~/bin/battery.bash"
+# See ./.bash_profile.example for details on how to build this script into you're prompt.
