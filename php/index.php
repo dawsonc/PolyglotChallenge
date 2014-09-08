9c32eeda0e682c72865123d76b8d32683a3cb3b0
Charles Dawson <charles.dwsn@gmail.com>
2014-09-07 22:34:37 -0400

    PHP

---
+++ php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP RNG</title>
+  </head>
+  <body>
+    <h1><?php
+      $lucky_number = rand();
+      echo "Your lucky number is <b>$lucky_number</b>"
+    ?>
+    </h1>
+  </body>
+</html>
